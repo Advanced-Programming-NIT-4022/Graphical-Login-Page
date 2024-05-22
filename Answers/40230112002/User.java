14e433f4dd8a1e72ec10dc8e2a4b187f23dbf097
xofError <sadraa772@gmail.com>
2024-05-22 20:54:36 +0330

    Added Hash_SHA_256 to User for Hashing the password of the user in case of a Successful Registration For Adding to File containing User Info

--- Answers/40230112002/User.java
+++ Answers/40230112002/User.java
@@ -1,3 +1,6 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class User {
 
     //Represents a user and contains methods for hashing and validating passwords
@@ -9,6 +12,32 @@ public class User {
         this.UserName = UserName;
         this.Email = Email;
         this.Password = Password;
+    }
+
+    private String Hash_SHA_256(String Password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            /*
+            This MessageDigest class provides applications the functionality of a message digest algorithm,
+             such as SHA-1 or SHA-256.
+              Message digests are secure one-way hash functions
+              that take arbitrary-sized data and output a fixed-length hash value.
+              Basically it's a Hash Function for SHA-1 to SHA-256
+             */
+            byte[] hashBytes = digest.digest(Password.getBytes());
+            //A digest is a short fixed-length value derived from some variable-length input
+            //A salt is a piece of random data added to a password before it is hashed and stored
+            StringBuilder builder = new StringBuilder();
+            for (byte b : hashBytes) {
+                builder.append(String.format("%02x", b));
+                //%02x means if your provided value is less than two digits then 0 will be prepended
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
 
@@ -38,4 +67,10 @@ public class User {
         Password = password;
     }
 
+    public static void main(String[] args) {
+        User user = new User("Sadra" , "sdjflsj" , "@Sadraahmadi83");
+        user.Hash_SHA_256(user.getPassword());
+    }
+
 }
+
