e05b4dfba45561487e3025ce56baba6629cda35d
Amir <salehamir708@gmail.com>
2024-05-22 21:55:19 +0330

    Email validator is added to validate email using regex

--- Answers/40230112081/Validator.java
+++ Answers/40230112081/Validator.java
@@ -1,3 +1,15 @@
+import java.sql.Connection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
+
+    public boolean emailValidator(String _email_)
+    {
+        Pattern pattern = Pattern.compile("^[^\\.][a-zA-Z0-9]+\\.?[^\\.][\\w]+@[^\\.][a-zA-Z]+\\.[a-z]{3,4}");
+        return pattern.matcher(_email_).find();
+    } // false : wrong email, true : correct email format
+
+
 
 }
