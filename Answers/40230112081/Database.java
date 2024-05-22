8e8c0ce7bf3e67c7bf2724bd7936f1de077dd4ee
Amir <salehamir708@gmail.com>
2024-05-22 22:42:02 +0330

    Insert method is added to project

--- Answers/40230112081/Database.java
+++ Answers/40230112081/Database.java
@@ -1,3 +1,37 @@
-public class Database {
+import javax.print.DocFlavor;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Calendar;
 
+public class Database {
+    private String password;
+    private String url;
+    private String name;
+    private String table;
+    public Database(String _url, String _password, String _name, String _table){
+        this.url = _url;
+        this.password = _password;
+        this.name = _name;
+        this.table = _table;
+    }
+
+    public void insertToTable(ArrayList<String> list){
+        int result;
+        try {
+            Connection connection = DriverManager.getConnection(this.url, this.name, this.password);
+            Statement statement = connection.createStatement();
+            result = statement.executeUpdate("INSERT INTO "+this.table+" VALUES(\""+list.get(0)+"\",\""+list.get(1)+
+                                            "\",\""+list.get(2)+"\")");
+            if(result == 1)
+                System.out.println("Insertion is successful");
+            else{
+                System.out.println("One error comes up");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
