27a70ea9a462539c0dd571c7d23be5ccd5ed9652
xofError <sadraa772@gmail.com>
2024-05-23 23:10:16 +0330

    Added a method to check the level of password security and complexity Haven't indicated the levels It can only pass the test when all conditions are met

--- Answers/40230112002/PasswordUtils.java
+++ Answers/40230112002/PasswordUtils.java
@@ -1,3 +1,33 @@
 public class PasswordUtils {
     //Contains utility methods for password strength evaluation and hashing
+
+    public static boolean isStrongPassword(String password) {
+        // Check if password meets the specified criteria
+        boolean containsLower = false;
+        boolean containsUpper = false;
+        boolean containsDigit = false;
+        boolean containsSpecial = false;
+        boolean isLongEnough = password.length() >= 8;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                containsLower = true;
+            } else if (Character.isUpperCase(ch)) {
+                containsUpper = true;
+            } else if (Character.isDigit(ch)) {
+                containsDigit = true;
+            } else if (isSpecialCharacter(ch)) {
+                containsSpecial = true;
+            }
+        }
+
+        return containsLower && containsUpper && containsDigit && containsSpecial && isLongEnough;
+    }
+
+    public static boolean isSpecialCharacter(char ch) {
+        // Define special characters
+        String specialCharacters = "@-_.";
+
+        return specialCharacters.contains(Character.toString(ch));
+    }
 }
