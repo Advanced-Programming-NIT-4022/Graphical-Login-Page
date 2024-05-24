ee4de0da97bb532f622206190f7f8acb9f296dc9
Yas Hassan Pour <yusshp1989@gmail.com>
2024-05-24 12:28:11 +0330

    LoginGUI class is half done

---
+++ Answers/yas_hassan_pour_40231712003/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginGUI gui = new LoginGUI();
+        LoginGUI.init();
+
+    }
+}
