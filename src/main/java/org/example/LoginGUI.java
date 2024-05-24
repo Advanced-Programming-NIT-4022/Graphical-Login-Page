029bf1d9e16615ab6dc01ef32d6d8b5811609ff4
daniel <d.boveiry@yahoo.com>
2024-05-24 14:21:51 +0330

    password and graphic

--- src/main/java/org/example/LoginGUI.java
+++ src/main/java/org/example/LoginGUI.java
@@ -1,5 +1,31 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 class LoginGUI{
+    LoginGUI(){
+        JFrame frame = new JFrame();
+        JButton button1 = new JButton("Login");
+        JButton button2 = new JButton("Register");
+        FlowLayout layout = new FlowLayout();
+        button1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                ;
+            }
+        });
+        button2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                ;
+            }
+        });
+        frame.add(button1);
+        frame.add(button2);
+        frame.setSize(500,500);
+        frame.setLayout(layout);
+        frame.setVisible(true);
+    }
 
 }
--- src/main/java/org/example/PasswordUtils.java
+++ src/main/java/org/example/PasswordUtils.java
@@ -1,5 +1,24 @@
 package org.example;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 class PasswordUtils{
+    private String hashedPassword;
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    void storePassword(String password){
+        hashedPassword = DigestUtils.sha256Hex(password);
+    }
+
+    boolean verifyPassword(String password){
+        return DigestUtils.sha256Hex(password).equals(hashedPassword);
+    }
+
+    //String passwordLevel(String password){
+        //if
+   // }
 
 }
