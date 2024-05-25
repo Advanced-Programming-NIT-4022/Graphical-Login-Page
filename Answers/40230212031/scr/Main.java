9708a3a09ae355aedbf0ae07027f38901de5d54e
Amirhoseinhmd83 <amirhosein.hamidii83@gmail.com>
2024-05-25 23:39:35 +0330

    Main class

--- Answers/40230212031/scr/Main.java
+++ Answers/40230212031/scr/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
-}
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the login ");
+            LoginGUI login = new LoginGUI();
+        }
+    }
+
