5ab3275ca12b56271cefc478f3ba563d768a4ba7
touka6 <hstkefayat@gmail.com>
2024-05-26 01:43:01 +0330

    done part2

--- Answers/40230112113/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        UserStore u = new UserStore();
-        User user = new User("hm", "h610", "Hasti_k83");
-        u.Save(user);
-        System.out.println(u.Read("h610", "Hasti_k83"));
-    }
-}
