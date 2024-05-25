0a22697b685c0c8abc1a97b5d109b72c3a6b72be
Amirhoseinhmd83 <amirhosein.hamidii83@gmail.com>
2024-05-25 23:26:53 +0330

    Email regex class done

--- Answers/40230212031/scr/EmailValidator.java
+++ Answers/40230212031/scr/EmailValidator.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class EmailValidator {
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile( "^[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@[A-Za-z0-9-]+\.[A-Za-z]{2,}$)";
+    public static boolean validateEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
 }
