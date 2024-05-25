668b6af715c0541b1517fa9841d125ac7d286b84
arshiammz <mohammadzade.arshia84@gmail.com>
2024-05-25 16:07:15 +0330

    passwordLevel method updated.

--- Answers/Login/src/main/java/Password.java
+++ Answers/Login/src/main/java/Password.java
@@ -4,29 +4,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Password {
-    public static String passwordLevel(String password){
-        Pattern patternLevel1 = Pattern.compile("\\b([A-Z]|[a-z]|\\d){4,8}\\b");
+    public static boolean passwordLevel(String password){
+        Pattern patternLevel1 = Pattern.compile("^[a-zA-Z0-9]+$");
         Matcher matcher1 = patternLevel1.matcher(password);
-        Pattern patternLevel2 = Pattern.compile("\\b([A-Z].*[a-z]){4,8}\\b");
+        Pattern patternLevel2 = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z]).+$");
         Matcher matcher2 = patternLevel2.matcher(password);
-        Pattern patternLevel3 = Pattern.compile("");
+        Pattern patternLevel3 = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).+$");
         Matcher matcher3 = patternLevel3.matcher(password);
-        Pattern patternLevel4 = Pattern.compile("");
+        Pattern patternLevel4 = Pattern.compile("^[\\w@\\-\\.]+$");
         Matcher matcher4 = patternLevel4.matcher(password);
-        Pattern patternLevel5 = Pattern.compile("\\b\\S{8,}\\b");
+        Pattern patternLevel5 = Pattern.compile("^.{9,}$");
         Matcher matcher5 = patternLevel5.matcher(password);
         if (matcher1.find()){
-            return "too weak";
+            return false;
         } else if (matcher2.find()){
-            return "weak";
-        } else if (matcher3.find()) {
-            return "normal";
+            return false;
+        } else if (matcher3.find()){
+            return true;
         } else if (matcher4.find()){
-            return "strong";
+            return true;
         } else if (matcher5.find()){
-            return "too strong";
+            return true;
         } else {
-            return "invalid";
+            return false;
         }
     }
 
