7b21adf50661e9eabbea54c05faa7bd8f5dfcfec
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-05-25 15:16:41 +0330

    created the user class.

---
+++ Answers/src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    private String Username;
+    private String Email;
+    private String Password;
+    public User(String username,String email,String password){
+        Username=username;
+        Email=email;
+        Password=password;
+    }
+}
