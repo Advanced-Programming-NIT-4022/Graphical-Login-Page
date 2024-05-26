ed031d110396692f2349337b06f13997a445097d
Amir.Homa <amirhomaunii@gmail.com>
2024-05-26 14:16:50 +0330

    add loginGUI class

--- Answers/Graphical-Login-Page/src/main/java/org/example/LoginGUI.java
+++ Answers/Graphical-Login-Page/src/main/java/org/example/LoginGUI.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class LoginGUI {
+    UserStore userStore = new UserStore();
     public void MainFrame() {
         JFrame frame = new JFrame();
         frame.setTitle("Welcome");
@@ -30,13 +31,18 @@ public class LoginGUI {
                 framesignin.setVisible(true);
             }
         });
-
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                JFrame framesignup = SignUpFrame();
+                framesignup.setVisible(true);
+            }
+        });
         panel.add(button1);
         panel.add(button2);
-
         frame.add(panel);
         frame.setVisible(true);
-
     }
     public JFrame SignInFrame() {
         JFrame frame = new JFrame();
@@ -51,6 +57,9 @@ public class LoginGUI {
         JButton button2 = new JButton();
         button2.setFocusable(false);
         button2.setText("Password :");
+        JButton button4 = new JButton();
+        button4.setFocusable(false);
+        button4.setText("Enter");
         JTextField textField1 = new JTextField(40);
         JTextField textField2 = new JTextField(40);
         button1.addActionListener(new ActionListener() {
@@ -80,11 +89,111 @@ public class LoginGUI {
                 MainFrame();
             }
         });
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input1 = textField1.getText();
+                String input2 = textField2.getText();
+                String sentense = input1 + "," + input2;
+                userStore.ReadFile();
+                //
+                System.out.println("successful");
+                userStore.WriteFile();
+            }
+        });
         panel.add(returnmain);
         panel.add(button1);
         panel.add(textField1);
         panel.add(button2);
         panel.add(textField2);
+        panel.add(button4);
+        frame.setResizable(false);
+        frame.add(panel);
+        return frame;
+    }
+    public JFrame SignUpFrame() {
+        userStore.ReadFile();
+        String[] word = new String[3];
+        JFrame frame = new JFrame();
+        frame.setTitle("Sign In");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        JPanel panel = new JPanel();
+        JButton button1 = new JButton();
+        button1.setFocusable(false);
+        button1.setText("UserName :");
+        JButton button2 = new JButton();
+        button2.setFocusable(false);
+        button2.setText("Password :");
+        JButton button3 = new JButton();
+        button3.setFocusable(false);
+        button3.setText("email :");
+        JButton button4 = new JButton();
+        button4.setFocusable(false);
+        button4.setText("Enter");
+        JTextField textField1 = new JTextField(40);
+        JTextField textField2 = new JTextField(40);
+        JTextField textField3 = new JTextField(40);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = textField1.getText();
+                JOptionPane.showMessageDialog(frame, input);
+//                word[0] = input;
+            }
+        });
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = textField3.getText();
+                JOptionPane.showMessageDialog(frame, input);
+//                word[2] = input;
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = textField2.getText();
+                JOptionPane.showMessageDialog(frame, input);
+//                word[1] = input;
+            }
+        });
+//        String sentense = word[0] + "," + "password" + "," + word[2];
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input1 = textField1.getText();
+                String input2 = textField2.getText();
+                String input3 = textField3.getText();
+                String sentense = input1 + "," + "password" + "," + input3;
+                userStore.ReadFile();
+                userStore.getUser().add(sentense);
+                System.out.println("successful");
+                userStore.WriteFile();
+            }
+        });
+        JButton returnmain = new JButton();
+        ImageIcon imageIcon = new ImageIcon("exit.png");
+        returnmain.setIcon(imageIcon);
+        returnmain.setToolTipText("Menu");
+        returnmain.setFocusable(true);
+        panel.setLayout(new GridLayout(8, 1));
+        returnmain.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                MainFrame();
+            }
+        });
+        panel.add(returnmain);
+        panel.add(button1);
+        panel.add(textField1);
+        panel.add(button2);
+        panel.add(textField2);
+        panel.add(button3);
+        panel.add(textField3);
+        panel.add(button4);
         frame.setResizable(false);
         frame.add(panel);
         return frame;
