54c0057c9ac49cb91a6a6eb4890ae434c0163ed8
vania <vania.v@gmail.com>
2024-05-28 01:14:59 +0330

    Edited EmailValidator

--- 40230212088/EmailValidator.java
+++ 40230212088/EmailValidator.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailValidator {
-    private static final String EMAIL_PATTERN = "^(.+)@(.+)$";
+    private static final String EMAIL_PATTERN = "^(.+^@)@(.+^@)$";
 
     public static boolean isValidEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
