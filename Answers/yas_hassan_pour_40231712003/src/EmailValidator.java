568e9b00a8950195284f14e1a21abb8ac49f453d
Yas Hassan Pour <yusshp1989@gmail.com>
2024-05-29 00:00:15 +0330

    EmailValidator kinda done

--- Answers/yas_hassan_pour_40231712003/src/EmailValidator.java
+++ Answers/yas_hassan_pour_40231712003/src/EmailValidator.java
@@ -1,7 +1,21 @@
+import java.util.regex.Pattern;
+
 public class EmailValidator {
     public void setEmailIn(String emailIn) {
         this.emailIn = emailIn;
     }
-
+    public int validate(String email)
+    {
+        int t=0;
+        if(Pattern.matches("(\\w(.)?\\w){1,}@[ge]mail.com", email))
+        {
+            t=1;
+        }
+        else
+        {
+            t=0;
+        }
+        return t;
+    }
     String emailIn;
 }
