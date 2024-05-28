1cb1a68392c8567bf338d7caa6245ff1c97ee102
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-05-28 22:40:12 +0330

    complete the search user function.

--- Answers/src/User.java
+++ Answers/src/User.java
@@ -1,3 +1,4 @@
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class User {
@@ -12,5 +13,11 @@ public class User {
     public void adduser(String username,String password,String email) throws SQLException {
         Db db=new Db();
         db.statement.executeUpdate("INSERT INTO loginpage (Name,Email,Password) VALUES ('"+username+"','"+email+"','"+password+"')");
+    }
+    public boolean searchuser(String name,String password) throws SQLException {
+        Db db=new Db();
+
+        ResultSet result=db.statement.executeQuery("SELECT * FROM loginpage WHERE Name='"+name+"' AND Password='"+password+"'");
+        return result.next();
     }
 }
