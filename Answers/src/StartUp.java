786ff08bf1506c11bef11c98a7ff2b1694759434
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-05-28 22:02:36 +0330

    login form completed

--- Answers/src/StartUp.java
+++ Answers/src/StartUp.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 
 public class StartUp extends JFrame {
     private JPasswordField password;
+    JTextField username;
+    JLabel title,message,passwordlbl,usernamelbl;
+    JButton butt;
     public StartUp(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800,600);
@@ -9,7 +12,28 @@ public class StartUp extends JFrame {
         this.setLocationRelativeTo(null);
         this.setLayout(null);
         password=new JPasswordField();
+
+
+
+        passwordlbl=new JLabel("Password");
+        passwordlbl.setBounds(200,250,100,40);
+
+        usernamelbl=new JLabel("Username");
+        usernamelbl.setBounds(200,200,100,40);
+
+        password.setBounds(260,250,200,40);
+
+
+        username=new JTextField("");
+        username.setBounds(260,200,200,40);;
+
+        butt=new JButton("sign in");
+        butt.setBounds(260,300,100,50);
         this.add(password);
+        this.add(passwordlbl);
+        this.add(usernamelbl);
+        this.add(username);
+        this.add(butt);
         this.setVisible(true);
     }
 }
