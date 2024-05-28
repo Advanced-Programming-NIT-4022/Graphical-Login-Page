c4c2ed346915427ba7a693b748fde61c023dd441
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-05-28 21:11:52 +0330

    making the startup swing

--- Answers/src/StartUp.java
+++ Answers/src/StartUp.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
 
 public class StartUp extends JFrame {
-    JFrame loginWindow;
-    StartUp(){
-        setTitle("Login Window");
-        
+    private JPasswordField password;
+    public StartUp(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800,600);
+        this.setTitle("GUI");
+        this.setLocationRelativeTo(null);
+        this.setLayout(null);
+        password=new JPasswordField();
+        this.add(password);
+        this.setVisible(true);
     }
 }
---
+++ Answers/src/Ui.java
@@ -0,0 +1,6 @@
+public class Ui {
+    public static void main(String[] args) {
+        StartUp start;
+        start=new StartUp();
+    }
+}
