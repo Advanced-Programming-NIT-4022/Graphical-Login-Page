bae1c6f0de833c84921647347b28b8c94a2b1b90
HJavid010 <HJavid0010@gmail.com>
2024-05-28 23:30:57 +0330

    created user handler class for storing user information

---
+++ Answers/40230112037/Graphical-Login-Page/src/main/java/UserHandler.java
@@ -0,0 +1,51 @@
+import com.sun.jdi.request.DuplicateRequestException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class UserHandler {
+    private final String filename;
+    private HashMap<String, User> users = new HashMap<>();
+
+    UserHandler(String filename) {
+        this.filename = filename;
+    }
+
+    void readUsersFromFile(String username) throws IOException {
+        File file = new File(filename);
+        try (Scanner scanner = new Scanner(file)) {
+            String nextLine;
+            HashMap<String, User> result = new HashMap<>();
+            User newUser;
+            while (scanner.hasNextLine()) {
+                nextLine = scanner.nextLine();
+                String[] nextLineSplit = nextLine.split(",");
+                if (nextLineSplit.length == 3 && (newUser = User.getValidatedUser(nextLineSplit[0], nextLineSplit[1], nextLineSplit[2])) != null && !result.containsKey(username)) {
+                    result.put(newUser.username, newUser);
+                }
+            }
+            users = result;
+        } catch (FileNotFoundException e) {
+            file.createNewFile();
+        }
+    }
+
+    void addUser(User newUser) throws IOException {
+        if (users.containsKey(newUser.username)) throw new DuplicateRequestException();
+        users.put(newUser.username, newUser);
+        File createFile = new File(filename);
+        createFile.createNewFile();
+
+        try (FileWriter file = new FileWriter(filename,true)) {
+            file.write(newUser.toString() + '\n');
+        }
+    }
+
+    User getUser(String username) {
+        return users.get(username);
+    }
+}
