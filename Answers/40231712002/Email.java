9b748a9ff68509bce79d950dbb066b9bf903b329
seyedkosarmousavi <seyedkosarmousavi2005@gmail.com>
2024-05-28 23:33:08 +0330

    add Java Project

---
+++ Answers/40231712002/Email.java
@@ -0,0 +1,10 @@
+import java.util.regex.Pattern;
+
+public class Email {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+
+    public static boolean isValid(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+}
