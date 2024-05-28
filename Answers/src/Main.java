88829a5e854108d561fc6d0e5592ad30ec4607e1
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-28 20:39:37 +0330

    create file in first step and set location of window in center

--- Answers/src/Main.java
+++ Answers/src/Main.java
@@ -1,11 +1,18 @@
 import javax.swing.JFrame;
 
 public class Main {
+    static LoginGUI login;
+
     public static void main(String[] args) {
-        LoginGUI login = new LoginGUI();
+        // file exist or not
+        UserStore.createFile();
+
+        // handle gui
+        login = new LoginGUI();
         login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         login.setResizable(false);
         login.setSize(700, 230);
+        login.setLocationRelativeTo(null);
         login.setVisible(true);
     }
 }
