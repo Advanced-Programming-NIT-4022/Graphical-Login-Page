31faa7fc3682fb91bf28ae9801cefb5c1c909474
HJavid010 <HJavid0010@gmail.com>
2024-05-30 09:47:55 +0330

    added passwordlevel patterns and method

--- Answers/40230112037/Graphical-Login-Page/src/main/java/Password.java
+++ Answers/40230112037/Graphical-Login-Page/src/main/java/Password.java
@@ -1,5 +1,21 @@
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.regex.Pattern;
+
+enum PasswordLevel {
+    VERY_WEAK(0),
+    WEAK(1),
+    INTERMEDIATE(2),
+    NORMAL(3),
+    STRONG(4),
+    VERY_STRONG(5);
+    int level;
+
+    PasswordLevel(int level) {
+
+    }
+}
+
 public class Password {
     private String hashedPassword;
 
@@ -34,13 +50,20 @@ public class Password {
     boolean verifyPassword(String password) {
         return hashedPassword.equals(DigestUtils.sha256Hex(password));
     }
-    // Todo : password level
-//    static int passwordLevel(String password){
-//        return
-//    }
+
+    static String[] passwordLevelPatterns = {"^(?=.*[a-z]).*$", "^(?=.*[A-Z]).*$", "^(?=.*[0-9]).*$", "^(?=.*[^a-zA-Z0-9]).*$", "^.{8,}$"};
+
 
     @Override
     public String toString() {
         return hashedPassword;
+    }
+
+    static int passwordLevel(String password) {
+        int level = 0;
+        for (String pattern : passwordLevelPatterns) {
+            if (Pattern.matches(pattern, password)) level++;
+        }
+        return level;
     }
 }
